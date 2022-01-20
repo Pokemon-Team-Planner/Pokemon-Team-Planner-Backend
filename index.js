a06aba712fabf5d5117c6d0b4fd7f7b5b2013dd6
Antti Alestalo <alestalo.antti@gmail.com>
2022-01-20 20:13:39 +0200

    prepare release to heroku

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
 
 app.use(express.static('public'))
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
