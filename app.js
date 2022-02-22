d3dbbe6c4badabaf933a23c3274e03081af9b1b0
Antti Alestalo <alestalo.antti@gmail.com>
2022-02-22 18:17:13 +0200

    refactor app to have separate middleware

--- app.js
+++ app.js
@@ -1,38 +1,19 @@
 const express = require('express')
-const morgan = require('morgan')
 const app = express()
-const logger = require('./utils/logger')
+//const logger = require('./utils/logger')
 const teamsRouter = require('./controllers/teams')
 const cors = require('cors')
+const middleware = require('./utils/middleware')
 app.use(cors())
 app.use(express.json())
 
-morgan.token('body', request => JSON.stringify(request.body))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
 app.use(express.static('build'))
 app.use(express.static('public'))
+app.use(middleware.requestLogger)
 
 app.use('/api/teams', teamsRouter)
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(unknownEndpoint)
-
-const errorHandler = (error, request, response, next) => {
-  logger.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
-
-app.use(errorHandler)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
