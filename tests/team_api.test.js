ccd6c2b570047165099fe0221ce07f19f3cb34d6
Antti Alestalo <alestalo.antti@gmail.com>
2022-03-08 19:58:15 +0200

    refactor tests to have better readability

--- tests/team_api.test.js
+++ tests/team_api.test.js
@@ -5,168 +5,178 @@ const app = require('../app')
 const api = supertest(app)
 const Team = require('../models/team')
 
-beforeEach(async () => {
-  await Team.deleteMany({})
-  await Team.insertMany(helper.initialTeams)
-})
+describe('when there are initially some teams saved', () => {
+  beforeEach(async () => {
+    await Team.deleteMany({})
+    await Team.insertMany(helper.initialTeams)
+  })
 
-test('teams are returned as json', async () => {
-  await api
-    .get('/api/teams')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+  test('teams are returned as json', async () => {
+    await api
+      .get('/api/teams')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-test('all teams are returned', async () => {
-  const response = await api.get('/api/teams')
+  test('all teams are returned', async () => {
+    const response = await api.get('/api/teams')
 
-  expect(response.body).toHaveLength(helper.initialTeams.length)
-})
+    expect(response.body).toHaveLength(helper.initialTeams.length)
+  })
 
-test('a specific team is within the returned teams', async () => {
-  const response = await api.get('/api/teams')
+  test('a specific team is within the returned teams', async () => {
+    const response = await api.get('/api/teams')
 
-  const contents = response.body.map(item => item.team)
+    const contents = response.body.map(item => item.team)
 
-  expect(contents).toContainEqual(
-    helper.initialTeams[0].team
-  )
-})
+    expect(contents).toContainEqual(
+      helper.initialTeams[0].team
+    )
+  })
 
-test('a valid team can be added ', async () => {
-  const newTeam = {
-    gameVersionPokedex: 'pokedex-firered.json',
-    date: new Date(),
-    team: [
-      { pokemonID: 1 },
-      { pokemonID: 2 },
-      { pokemonID: 3 },
-      { pokemonID: 4 },
-      { pokemonID: 5 },
-      { pokemonID: 6 }
-    ]
-  }
+  describe('adding a new team', () => {
+    test('succeeds with valid data ', async () => {
+      const newTeam = {
+        gameVersionPokedex: 'pokedex-firered.json',
+        date: new Date(),
+        team: [
+          { pokemonID: 1 },
+          { pokemonID: 2 },
+          { pokemonID: 3 },
+          { pokemonID: 4 },
+          { pokemonID: 5 },
+          { pokemonID: 6 }
+        ]
+      }
+  
+      await api
+        .post('/api/teams')
+        .send(newTeam)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+  
+      const teamsAtEnd = await helper.teamsInDb()
+  
+      const teams = teamsAtEnd.map(item => item.team)
+  
+      expect(teamsAtEnd).toHaveLength(helper.initialTeams.length + 1)
+      expect(teams).toContainEqual(
+        newTeam.team
+      )
+    })
+  
+    test('fails with status code 400 if no pokemon in team', async () => {
+      const newTeam = {
+        gameVersionPokedex: 'pokedex-firered.json',
+        date: new Date(),
+        team: []
+      }
+  
+      await api
+        .post('/api/teams')
+        .send(newTeam)
+        .expect(400)
+  
+      const response = await api.get('/api/teams')
+  
+      expect(response.body).toHaveLength(helper.initialTeams.length)
+    })
+  
+    test('fails with status code 400 if more than 6 pokemon in team', async () => {
+      const newTeam = {
+        gameVersionPokedex: 'pokedex-firered.json',
+        date: new Date(),
+        team: [
+          { pokemonID: 1 },
+          { pokemonID: 2 },
+          { pokemonID: 3 },
+          { pokemonID: 4 },
+          { pokemonID: 5 },
+          { pokemonID: 6 },
+          { pokemonID: 7 }
+        ]
+      }
+  
+      await api
+        .post('/api/teams')
+        .send(newTeam)
+        .expect(400)
+  
+      const response = await api.get('/api/teams')
+  
+      expect(response.body).toHaveLength(helper.initialTeams.length)
+    })
+  })
 
-  await api
-    .post('/api/teams')
-    .send(newTeam)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+  describe('viewing a team', () => {
+    test('succeeds with valid id', async () => {
+      const teamsAtStart = await helper.teamsInDb()
+  
+      const teamToView = teamsAtStart[0]
+  
+      const resultTeam = await api
+        .get(`/api/teams/${teamToView.id}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+  
+      const processedTeamToView = JSON.parse(JSON.stringify(teamToView)) //tranforms date-object to string
+  
+      expect(resultTeam.body).toEqual(processedTeamToView)
+    })
+  })
+  
+  describe('deleting a team', () => {
+    test('succeeds with status code 204 if id is valid', async () => {
+      const teamsAtStart = await helper.teamsInDb()
+      const teamToDelete = teamsAtStart[0]
+  
+      await api
+        .delete(`/api/teams/${teamToDelete.id}`)
+        .expect(204)
+  
+      const teamsAtEnd = await helper.teamsInDb()
+  
+      expect(teamsAtEnd).toHaveLength(
+        helper.initialTeams.length - 1
+      )
+  
+      const teams = teamsAtEnd.map(item => item.team)
+  
+      expect(teams).not.toContainEqual(teamToDelete.team)
+    })
+  })
 
-  const teamsAtEnd = await helper.teamsInDb()
-
-  const teams = teamsAtEnd.map(item => item.team)
-
-  expect(teamsAtEnd).toHaveLength(helper.initialTeams.length + 1)
-  expect(teams).toContainEqual(
-    newTeam.team
-  )
-})
-
-test('team without pokemon is not added', async () => {
-  const newTeam = {
-    gameVersionPokedex: 'pokedex-firered.json',
-    date: new Date(),
-    team: []
-  }
-
-  await api
-    .post('/api/teams')
-    .send(newTeam)
-    .expect(400)
-
-  const response = await api.get('/api/teams')
-
-  expect(response.body).toHaveLength(helper.initialTeams.length)
-})
-
-test('team with more than 6 pokemon is not added', async () => {
-  const newTeam = {
-    gameVersionPokedex: 'pokedex-firered.json',
-    date: new Date(),
-    team: [
-      { pokemonID: 1 },
-      { pokemonID: 2 },
-      { pokemonID: 3 },
-      { pokemonID: 4 },
-      { pokemonID: 5 },
-      { pokemonID: 6 },
-      { pokemonID: 7 }
-    ]
-  }
-
-  await api
-    .post('/api/teams')
-    .send(newTeam)
-    .expect(400)
-
-  const response = await api.get('/api/teams')
-
-  expect(response.body).toHaveLength(helper.initialTeams.length)
-})
-
-test('a specific team can be viewed', async () => {
-  const teamsAtStart = await helper.teamsInDb()
-
-  const teamToView = teamsAtStart[0]
-
-  const resultTeam = await api
-    .get(`/api/teams/${teamToView.id}`)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  const processedTeamToView = JSON.parse(JSON.stringify(teamToView)) //tranforms date-object to string
-
-  expect(resultTeam.body).toEqual(processedTeamToView)
-})
-
-test('a team can be deleted', async () => {
-  const teamsAtStart = await helper.teamsInDb()
-  const teamToDelete = teamsAtStart[0]
-
-  await api
-    .delete(`/api/teams/${teamToDelete.id}`)
-    .expect(204)
-
-  const teamsAtEnd = await helper.teamsInDb()
-
-  expect(teamsAtEnd).toHaveLength(
-    helper.initialTeams.length - 1
-  )
-
-  const teams = teamsAtEnd.map(item => item.team)
-
-  expect(teams).not.toContainEqual(teamToDelete.team)
-})
-
-test('a team can be updated', async () => {
-  const teamsAtStart = await helper.teamsInDb()
-  const teamToUpdate = teamsAtStart[0]
-  const updatedTeam = {
-    team: [
-      { pokemonID: 9 },
-      { pokemonID: 9 },
-      { pokemonID: 9 },
-      { pokemonID: 9 },
-      { pokemonID: 9 },
-      { pokemonID: 9 }
-    ]
-  }
-
-  await api
-    .put(`/api/teams/${teamToUpdate.id}`)
-    .send(updatedTeam)
-    .expect(200)
-
-  const teamsAtEnd = await helper.teamsInDb()
-
-  expect(teamsAtEnd).toHaveLength(
-    helper.initialTeams.length
-  )
-
-  const teams = teamsAtEnd.map(item => item.team)
-
-  expect(teams).toContainEqual(updatedTeam.team)
+  describe('updating a team', () => {
+    test('succeeds with valid team and id', async () => {
+      const teamsAtStart = await helper.teamsInDb()
+      const teamToUpdate = teamsAtStart[0]
+      const updatedTeam = {
+        team: [
+          { pokemonID: 9 },
+          { pokemonID: 9 },
+          { pokemonID: 9 },
+          { pokemonID: 9 },
+          { pokemonID: 9 },
+          { pokemonID: 9 }
+        ]
+      }
+  
+      await api
+        .put(`/api/teams/${teamToUpdate.id}`)
+        .send(updatedTeam)
+        .expect(200)
+  
+      const teamsAtEnd = await helper.teamsInDb()
+  
+      expect(teamsAtEnd).toHaveLength(
+        helper.initialTeams.length
+      )
+  
+      const teams = teamsAtEnd.map(item => item.team)
+  
+      expect(teams).toContainEqual(updatedTeam.team)
+    })
+  })
 })
 
 afterAll(() => {
