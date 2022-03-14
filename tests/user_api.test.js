1622f3a93fef6c635d7283567dc96d32ed9cb143
Antti Alestalo <alestalo.antti@gmail.com>
2022-03-14 15:29:51 +0200

    refactor user api tests for better readability

--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -16,132 +16,136 @@ describe('when there is initially one user at db', () => {
     await user.save()
   })
 
-  test('creation succeeds with a fresh username', async () => {
-    const usersAtStart = await helper.usersInDb()
+  describe('creation succeeds if', () => {
+    test('username is fresh and unique', async () => {
+      const usersAtStart = await helper.usersInDb()
 
-    const newUser = {
-      username: 'alestan',
-      name: 'AnttiA',
-      password: 'salainen',
-    }
+      const newUser = {
+        username: 'alestan',
+        name: 'AnttiA',
+        password: 'salainen',
+      }
 
-    await api
-      .post('/api/users')
-      .send(newUser)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
+      await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-    const usersAtEnd = await helper.usersInDb()
-    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+      const usersAtEnd = await helper.usersInDb()
+      expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
 
-    const usernames = usersAtEnd.map(u => u.username)
-    expect(usernames).toContain(newUser.username)
+      const usernames = usersAtEnd.map(u => u.username)
+      expect(usernames).toContain(newUser.username)
+    })
+
+    test('username contains valid characters', async () => {
+      const usersAtStart = await helper.usersInDb()
+
+      const newUser = {
+        username: '_Antti.123_',
+        name: 'AnttiA',
+        password: 'salainen',
+      }
+
+      await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const usersAtEnd = await helper.usersInDb()
+      expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+      const usernames = usersAtEnd.map(u => u.username)
+      expect(usernames).toContain(newUser.username)
+    })
   })
 
-  test('creation succeeds with valid characters in username', async () => {
-    const usersAtStart = await helper.usersInDb()
+  describe('creation fails with proper statuscode and message if', () => {
+    test('username already taken', async () => {
+      const usersAtStart = await helper.usersInDb()
 
-    const newUser = {
-      username: '_Antti.123_',
-      name: 'AnttiA',
-      password: 'salainen',
-    }
+      const newUser = {
+        username: 'root',
+        name: 'Superuser',
+        password: 'salainen',
+      }
 
-    await api
-      .post('/api/users')
-      .send(newUser)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
+      const result = await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
 
-    const usersAtEnd = await helper.usersInDb()
-    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+      expect(result.body.error).toContain('username must be unique')
 
-    const usernames = usersAtEnd.map(u => u.username)
-    expect(usernames).toContain(newUser.username)
-  })
+      const usersAtEnd = await helper.usersInDb()
+      expect(usersAtEnd).toHaveLength(usersAtStart.length)
+    })
 
-  test('creation fails with proper statuscode and message if username already taken', async () => {
-    const usersAtStart = await helper.usersInDb()
+    test('username is shorter than 6 chars', async () => {
+      const usersAtStart = await helper.usersInDb()
 
-    const newUser = {
-      username: 'root',
-      name: 'Superuser',
-      password: 'salainen',
-    }
+      const newUser = {
+        username: 'admin',
+        name: 'Admin',
+        password: 'salainen',
+      }
 
-    const result = await api
-      .post('/api/users')
-      .send(newUser)
-      .expect(400)
-      .expect('Content-Type', /application\/json/)
+      const result = await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
 
-    expect(result.body.error).toContain('username must be unique')
+      expect(result.body.error).toContain('username must be 6 characters at minimum')
 
-    const usersAtEnd = await helper.usersInDb()
-    expect(usersAtEnd).toHaveLength(usersAtStart.length)
-  })
+      const usersAtEnd = await helper.usersInDb()
+      expect(usersAtEnd).toHaveLength(usersAtStart.length)
+    })
 
-  test('creation fails with proper status code and message if username is shorter than 6 chars', async () => {
-    const usersAtStart = await helper.usersInDb()
+    test('username is longer than 20 chars', async () => {
+      const usersAtStart = await helper.usersInDb()
 
-    const newUser = {
-      username: 'admin',
-      name: 'Admin',
-      password: 'salainen',
-    }
+      const newUser = {
+        username: 'adminadminadminadmin1',
+        name: 'Admin',
+        password: 'salainen',
+      }
 
-    const result = await api
-      .post('/api/users')
-      .send(newUser)
-      .expect(400)
-      .expect('Content-Type', /application\/json/)
+      const result = await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
 
-    expect(result.body.error).toContain('username must be 6 characters at minimum')
+      expect(result.body.error).toContain('username must be 20 characters at maximum')
 
-    const usersAtEnd = await helper.usersInDb()
-    expect(usersAtEnd).toHaveLength(usersAtStart.length)
-  })
+      const usersAtEnd = await helper.usersInDb()
+      expect(usersAtEnd).toHaveLength(usersAtStart.length)
+    })
 
-  test('creation fails with proper status code and message if username is longer than 20 chars', async () => {
-    const usersAtStart = await helper.usersInDb()
+    test('username contains invalid characters', async () => {
+      const usersAtStart = await helper.usersInDb()
 
-    const newUser = {
-      username: 'adminadminadminadmin1',
-      name: 'Admin',
-      password: 'salainen',
-    }
+      const newUser = {
+        username: '!?/&%¤#"!#¤/)=',
+        name: 'Admin',
+        password: 'salainen',
+      }
 
-    const result = await api
-      .post('/api/users')
-      .send(newUser)
-      .expect(400)
-      .expect('Content-Type', /application\/json/)
+      const result = await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
 
-    expect(result.body.error).toContain('username must be 20 characters at maximum')
+      expect(result.body.error).toContain('username must contain only letters, numbers, _ and .')
 
-    const usersAtEnd = await helper.usersInDb()
-    expect(usersAtEnd).toHaveLength(usersAtStart.length)
-  })
-
-  test('creation fails with proper status code and message if username contains invalid characters', async () => {
-    const usersAtStart = await helper.usersInDb()
-
-    const newUser = {
-      username: '!?/&%¤#"!#¤/)=',
-      name: 'Admin',
-      password: 'salainen',
-    }
-
-    const result = await api
-      .post('/api/users')
-      .send(newUser)
-      .expect(400)
-      .expect('Content-Type', /application\/json/)
-
-    expect(result.body.error).toContain('username must contain only letters, numbers, _ and .')
-
-    const usersAtEnd = await helper.usersInDb()
-    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+      const usersAtEnd = await helper.usersInDb()
+      expect(usersAtEnd).toHaveLength(usersAtStart.length)
+    })
   })
 })
 
